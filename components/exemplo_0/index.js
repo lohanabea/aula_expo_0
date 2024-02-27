5aceb3b1bd8247145fc5e8bc76971ab5f9dc34f1
lohanabea <160647313+lohanabea@users.noreply.github.com>
2024-02-27 15:24:54 -0300

    aula27-02

---
+++ components/exemplo_0/index.js
@@ -0,0 +1,11 @@
+import{ View,Text }from 'react-native'
+
+function Exemplo_0 (){
+    return(
+        <View>
+            <text>Minha primeira tela</Text>
+        </View>
+    );
+}
+
+export defaut Exemplo_0;
