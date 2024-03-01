d9357fa36a3eba8babb71b099bc5913dc85a1ccf
lohanabea <160647313+lohanabea@users.noreply.github.com>
2024-03-01 09:48:32 -0300

    aula01-03

--- components/exemplo_0/index.js
+++ components/exemplo_0/index.js
@@ -3,9 +3,9 @@ import{ View,Text }from 'react-native'
 function Exemplo_0 (){
     return(
         <View>
-            <text>Minha primeira tela</Text>
+            <Text style={{color:'#FFF'}}>Minha primeira tela</Text>
         </View>
     );
 }
 
-export defaut Exemplo_0;
+export default Exemplo_0;
