aee0211a968ad0a14dcbd6900eacdcbb309ace21
lohanabea <160647313+lohanabea@users.noreply.github.com>
2024-03-19 15:23:16 -0300

    aula 19-03

---
+++ components/exemplo_2/index.js
@@ -0,0 +1,11 @@
+import{ View,Text }from 'react-native'
+import styles from './styles';
+
+function Exemplo2 (){
+    return(
+        <View style={styles.container}>
+            <Text >Exemplo2</Text>
+        </View>       
+    )
+};
+export default Exemplo2;
