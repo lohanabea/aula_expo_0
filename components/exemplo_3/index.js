2c92bff85526c72d9b2e16c8093a31cc84c992c7
lohanabea <160647313+lohanabea@users.noreply.github.com>
2024-05-07 15:23:56 -0300

    aula 07-05

--- components/exemplo_3/index.js
+++ components/exemplo_3/index.js
@@ -1,13 +1,20 @@
-import{ View,Text, Pressable }from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 
 import styles from './styles';
 
-export default function Exemplo_3(){
-    return(
+export default function Exemplo_3() {
+    return (
         <View style={styles.container}>
             <Text style={styles.titulo}>Exemplo 3</Text>
-            <Pressable onPress={() => alert ('Oi')} style={styles.botao}>
-                <Text>Ex. botão</Text>
+
+            <Pressable
+                onPress={() => alert('Oi')}
+                style={({ pressed }) => pressed ?
+                   [ styles.botao, styles.botaoPress]
+                    :
+                    styles.botao}
+            >
+                <Text style={styles.txtBotao}>Ex. botão</Text>
             </Pressable>
         </View>
     );
--- components/exemplo_3/styles.js
+++ components/exemplo_3/styles.js
@@ -1,26 +1,36 @@
-import{ StyleSheet }from 'react-native';
+import { StyleSheet } from 'react-native';
 
 
-const styles= StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:'#D1C4E9',
-        borderRadius:20,
-        width:'100%',
-        alignItems:'center',
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#D1C4E9',
+        borderRadius: 20,
+        width: '100%',
+        alignItems: 'center',
 
     },
-    titulo:{
-        fontSize:30,
-        fontWeight:'bold',
-        marginVertical:20,
-        color:'#673AB7',
+    titulo: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginVertical: 20,
+        color: '#673AB7',
     },
-    botao:{
-        borderWidth:3,
-        padding:10,
-        borderRadius:20,
-        alignItems:'center',
+    botao: {
+        borderWidth: 3,
+        padding: 10,
+        width: '80%',
+        borderRadius: 20,
+        alignItems: 'center',
+        borderColor: '#3c1752',
+
+    },
+    txtBotao: {
+        fontSize: 25,
+        color: '#673AB7',
+    },
+    botaoPress:{
+        backgroundColor:'#3c1752',
     },
 });
 export default styles;
